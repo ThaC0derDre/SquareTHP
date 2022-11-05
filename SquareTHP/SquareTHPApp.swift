c52faf22fe9087db59688cd848a757e8802a9512
AndresGutierrez <c0dingw.dre@gmail.com>
2022-11-05 09:49:56 -0700

    Initial Commit

---
+++ SquareTHP/SquareTHPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareTHPApp.swift
+//  SquareTHP
+//
+//  Created by Andres Gutierrez on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SquareTHPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
